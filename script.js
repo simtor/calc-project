a15c883f582cf581dcd5d089ed9f0f70c23c8fd3
simtor <simone.ram@ada.ac.uk>
2020-06-15 13:46:54 +0100

    created functions for operations

---
+++ script.js
@@ -0,0 +1,40 @@
+class calculator{
+    constructor(previousoperandtextElement, currentorerandtextElement){
+    this.previousoperandtextElement = previousoperandtextElement
+    this.currentorerandtextElement = currentorerandtextElement
+}
+clear(){
+
+
+}
+delete(){
+
+}
+
+appendNumber(){
+
+}
+
+choosenOperation(operation){
+
+}
+
+compute(){
+
+}
+
+updateDisplay(){
+
+}
+
+}
+
+const numberButttons = document.querySelectionAll('[data-number]')
+const operationButttons = document.querySelectionAll('[data-operation]')
+const equalsbutton = Document.queryselector('[data-equals]')
+const deletebutton = Document.queryselector('[data-delete]')
+const allcleanbutton = Document.queryselector('[data-all-clear]')
+const previousoperandtextElement = Document.queryselector('[data-previous-operand]')
+const currentorerandtextElement = Document.queryselector('[data-current-orerand]')
+
+
